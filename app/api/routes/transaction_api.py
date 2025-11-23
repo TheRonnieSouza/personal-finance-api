ec54a851058edeb4e5f1e12f15bf4c38d5fa9a18
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-11-23 12:20:05 -0300

    feat: add route put transaction

--- app/api/routes/transaction_api.py
+++ app/api/routes/transaction_api.py
@@ -3,45 +3,50 @@ from app.transactions.transaction import Transaction
 from app.transactions.i_transaction_repository import ITransactionRepository
 from app.transactions.create.create_transaction_command import  CreateTransactionCommand
 from app.transactions.create.create_transaction_command_handler import  CreateTransactionCommandHandler
-from app.infra.transaction_repository import TransactionRepository
+from app.transactions.update.update_command import UpdateTransactionCommand
+from app.transactions.update.update_command_handler import UpdateTransactionCommandHandler
 from app.infra.dependencies import get_transaction_repository
 from app.api.models.transaction_response import TransactionResponse
+import logging
 
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
-'''
-@router.get("/v2/transaction/{id}")
-def get_transaction(id: int = Path(description="Transaction id"),
-                    phone_number: str = Query(None, default="phone number")               
-                    )
-                    '''
-#TODO create view model                    
-@router.post("/v2/transaction", response_model= TransactionResponse, status_code=201, tags=["Transactions"])
+tags= ["Transactions"]                   
+                              
+@router.post("/v2/transaction", response_model= TransactionResponse, status_code=status.HTTP_201_CREATED, tags=tags)
 def create_transaction(payload: CreateTransactionCommand, repository: ITransactionRepository = Depends(get_transaction_repository)):
     print("request post v2/transaction realizada!")
     try:
-       handler = CreateTransactionCommandHandler(repository)
-    
+       logger.info(f"Received PUT request for transaction {id}")
+       handler = CreateTransactionCommandHandler(repository)    
        result = handler.handle(payload)
        
-       return TransactionResponse(
-            id=result.id,
-            name=result.name,
-            date=result.date,
-            amount=result.amount,
-            currency=result.currency,
-            type=result.type,
-            description=result.description,
-            category=result.category
-        )
+       logger.info(f"Updated successful, returning response")
+       return TransactionResponse.model_validate(result)
     
     except ValueError as e:
+        logger.error(f"Validation error: {str(e)}")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=status.HTTP_400_NOT_FOUND,
             detail=str(e)
         )
     except Exception as e:
+        logger.exception(f"Unexpected error updating transaction id={id}: {str(e)} ")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=str(e)
+            detail=f"An error occurred while updating the transaction: {str(e)}"
         )
+
+@router.put("/v2/transaction/{id}", response_model= TransactionResponse, status_code=status.HTTP_200_OK, tags=tags)
+def update_transaction(id: int, payload: UpdateTransactionCommand, repository: ITransactionRepository = Depends(get_transaction_repository)):
+    try:
+        handler = UpdateTransactionCommandHandler(repository)
+        
+        result = handler.handle(id, payload)
+        return TransactionResponse.model_validate(result)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
