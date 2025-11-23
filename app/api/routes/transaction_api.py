5a71ccefd61d4f1fdc1ff8516703c33b131773b9
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-11-23 12:31:43 -0300

    fix: status code

--- app/api/routes/transaction_api.py
+++ app/api/routes/transaction_api.py
@@ -28,7 +28,7 @@ def create_transaction(payload: CreateTransactionCommand, repository: ITransacti
     except ValueError as e:
         logger.error(f"Validation error: {str(e)}")
         raise HTTPException(
-            status_code=status.HTTP_400_NOT_FOUND,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
         )
     except Exception as e:
